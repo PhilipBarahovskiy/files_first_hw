f36113a7f0ab2b713c904d64f8d6565bfa41a0a3
Philip Barahovskiy <barahovskij@yandex.ru>
2020-10-13 12:23:38 +0300

    Cook_book and Recipes added

---
+++ cook_book.py
@@ -0,0 +1,22 @@
+def format_ingredient(s):
+  return dict(zip(['ingredient_name', 'quantity', 'measure'], s.replace('\n','').split(' | ')))
+
+def format_dish(dish,ingredients):
+  d = {dish:[]}
+  for i in ingredients:
+    d[dish].append(format_ingredient(i))
+  return d
+
+cook_book = {}
+with open('recipes.txt') as f:
+  data = f.readlines()
+ 
+i = 0
+while i < len(data):
+  dish = data[i].replace('\n','')
+  length = int(data[i+1])
+  ingredients = data[(i+2):(i+2+length)]
+  cook_book.update(format_dish(dish,ingredients))
+  i += length + 3
+
+print(cook_book)
