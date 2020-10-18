0e6e17f67eeec6532cf373a0e3bc764fc4331260
Philip Barahovskiy <barahovskij@yandex.ru>
2020-10-18 18:32:06 +0300

    encoding added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 files_data = [['1.txt'],['2.txt'],['3.txt']]
 for file_data in files_data:
-  with open(file_data[0]) as f:
+  with open(file_data[0], encoding = "utf-8") as f:
     text = f.readlines()
     text[-1] += '\n'
     file_data += text
@@ -14,5 +14,5 @@ for file_data in files_data:
   file_data[1] = f'{file_data[1]}\n'
   ret+=file_data
 
-with open('res.txt', 'w') as f:
+with open('res.txt', 'w', encoding = "utf-8" ) as f:
   f.writelines(ret)
